6a4a5e4e2cdb1e218f3d5134de71c9f4286450b7
ChristianChiarulli <chrisatmachine@gmail.com>
2020-01-10 00:31:30 -0500

    updated expo 36

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { f, auth, database } from './config/config.js'
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { f, auth, database } from "./config/config.js";
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     // this.registerUser('testemailaddress@gmail.com', 'fakepassword')
 
     f.auth().onAuthStateChanged(function(user) {
       if (user) {
-        console.log('Logged in', user)
+        console.log("Logged in", user);
       } else {
-        console.log('Logged out')
+        console.log("Logged out");
       }
-    })
-
+    });
   }
 
-  async fun
-
   registerUser = (email, password) => {
-    console.log(email, password)
+    console.log(email, password);
     auth
       .createUserWithEmailAndPassword(email, password)
       .then(userObj => console.log(email, password, userObj))
-      .catch(error => console.log('error logging in', error))
-  }
+      .catch(error => console.log("error logging in", error));
+  };
 
   // auth
   //   .signOut()
@@ -42,15 +38,15 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
-})
+});
